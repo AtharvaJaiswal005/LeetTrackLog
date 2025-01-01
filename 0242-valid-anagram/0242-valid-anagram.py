c099b67579a1d8aad28995653eb475e5b9eb9d7c
Atharva Jaiswal <92455570+AtharvaJaiswal005@users.noreply.github.com>
2025-01-01 15:00:52 +0530

    Time: 22 ms (55.98%), Space: 14.3 MB (5.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,4 +5,10 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return sorted(s)==sorted(t)
+        if len(s)!=len(t):
+            return False
+        d1={};d2={}
+        for i in range(len(s)):
+            d1[s[i]]=d1.get(s[i],0)+1
+            d2[t[i]]=d2.get(t[i],0)+1
+        return d1==d2
