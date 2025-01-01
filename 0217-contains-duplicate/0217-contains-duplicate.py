57d2e13b1bf00d8062dc30113747fc337d420de1
Atharva Jaiswal <92455570+AtharvaJaiswal005@users.noreply.github.com>
2025-01-01 11:27:05 +0530

    Time: 7 ms (90.88%), Space: 25.9 MB (13.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,11 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        s=set(nums)
-        f={i:0 for i in s}
-        for i in nums:
-            f[i]+=1
-        for i in f.values():
-            if i>1:
-                return True
-        return False
+        return len(set(nums)) != len(nums)
