5ed59e56a4797c4d075361bad137421273b10468
Atharva Jaiswal <92455570+AtharvaJaiswal005@users.noreply.github.com>
2025-01-14 08:02:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,24 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        from collections import defaultdict
-        
-        # Step 1: Count the frequency of each number
-        freq = {}
-        for num in nums:
-            freq[num] = freq.get(num, 0) + 1
-        
-        # Step 2: Create a frequency bucket where keys are counts
-        bucket = defaultdict(list)
-        for num, count in freq.items():
-            bucket[count].append(num)
-        
-        # Step 3: Sort the frequencies in descending order
-        sorted_freqs = sorted(bucket.keys(), reverse=True)
-        
-        # Step 4: Collect the top K frequent elements
-        result = []
-        for freq_count in sorted_freqs:
-            result.extend(bucket[freq_count])  # Add all elements with the same frequency
-            if len(result) >= k:  # Stop if we already have k elements
-                return result[:k]  # Return exactly k elements
+        freq={}
+        t=[[] for i in range(len(nums)+1)]
+        res=[]
+        for i in nums:
+            freq[i]=freq.get(i,0)+1
+        for i,j in freq.items():
+            t[j].append(i)
+        for i in range(len(t)-1,-1,-1):
+            if t[i] and k!=0:
+                print(t[i])
+                res.extend(t[i])
+                k-=1
+        return res
